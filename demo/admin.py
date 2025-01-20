dceb8ed0fcfa7005f51187f0e115475931b84a46
linmingwang <linmingwang@ata.net.cn>
2025-01-20 10:24:00 +0800

    feat: Add BookAdmin configuration to Django admin
    - Introduced a new admin interface for the Book model, enhancing the management capabilities within the Django admin panel.
    - Configured list display, filtering options, and search fields to improve usability and data accessibility for administrators.
    - Implemented fieldsets for organized layout of fields, including sections for dates and administrators, facilitating better data entry and management.
    - These changes aim to streamline the administration of book records and improve overall user experience in the admin interface.

--- demo/admin.py
+++ demo/admin.py
@@ -1,1 +1,28 @@
 # Register your models here.
+
+from django.contrib import admin
+from demo.models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'isbn', 'category', 'author', 'publisher', 'price', 'is_active', 'publication_date']
+    list_filter = ['category', 'is_active', 'publisher']
+    search_fields = ['name', 'isbn', 'author', 'publisher']
+    date_hierarchy = 'publication_date'
+    filter_horizontal = ['managers', 'managers2']
+    raw_id_fields = ['admin', 'admin2']
+    readonly_fields = ['created_time', 'updated_time']
+    fieldsets = [
+        (None, {
+            'fields': ['name', 'isbn', 'category', 'author', 'publisher', 'price', 'is_active']
+        }),
+        ('Dates', {
+            'fields': ['publication_date', 'created_time', 'updated_time']
+        }),
+        ('Administrators', {
+            'fields': ['admin', 'admin2', 'managers', 'managers2']
+        }),
+        ('Files', {
+            'fields': ['cover', 'avatar', 'book_file']
+        }),
+    ]
